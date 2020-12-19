0de7838234800282bbe3926a25dc37a8e29344af
jaskokoyn <jack@jaskokoyn.com>
2020-12-19 14:49:50 -0500

    upgrade: Angular 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  email = '';
-
-  handleSubmit() {
-    console.log(this.email);
-  }
+  title = 'email-form';
 }
